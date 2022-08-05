f1061b7be304e122d256ddbabffa292d8c813ee9
Chandra Teja Reddy <53616439+teja016@users.noreply.github.com>
2022-08-05 17:34:26 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,8 @@
+import unittest
+from bmi import person
+class TestBmi(unittest.TestCase):
+    def test_set_bmi_data_and_overweight(self):
+        #Assert
+        self.assertTrue(True)
+if __name__ == '__main__':
+    unittest.main()
